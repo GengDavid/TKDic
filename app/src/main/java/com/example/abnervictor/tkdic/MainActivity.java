f35e6f85755a11e6d880d457e887b5023543f3e4
AbnerVictor <victoryyxfei@gmail.com>
2017-11-19 01:28:27 +0800

    search_bar

--- app/src/main/java/com/example/abnervictor/tkdic/MainActivity.java
+++ app/src/main/java/com/example/abnervictor/tkdic/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.abnervictor.tkdic;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +13,12 @@ public class MainActivity extends AppCompatActivity {
     private ImageView folder;
     private ImageView privatecollect;
 
+    private SearchBar Searchbar;
+    private ImageView search_button;
+    private EditText search_text;
+    private View search_content;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
         findView();
         SetNavigationBarListener();
+        SetSearchBarListener();
 
     }
 
@@ -28,8 +36,14 @@ public class MainActivity extends AppCompatActivity {
         folder = findViewById(R.id.navigation_bar_folder);
         privatecollect = findViewById(R.id.navigation_bar_privatecollect);
 
+        search_button = findViewById(R.id.search_button);
+        search_text = findViewById(R.id.search_text);
+        search_content = findViewById(R.id.search_content);
+
         Navigationbar = new NavigationBar(home,folder,privatecollect);
         Navigationbar.UpdateNavigationBarState(1);//初始化导航栏
+
+        Searchbar = new SearchBar(search_button,search_text,search_content);
 
     }
 
@@ -54,5 +68,12 @@ public class MainActivity extends AppCompatActivity {
         });
     }//导航栏监听器
 
-
+    private void SetSearchBarListener(){
+        search_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Searchbar.Reversefocusing();
+            }
+        });
+    }
 }
---
+++ app/src/main/java/com/example/abnervictor/tkdic/SearchBar.java
@@ -0,0 +1,86 @@
+package com.example.abnervictor.tkdic;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+/**
+ * Created by abnervictor on 2017/11/18.
+ */
+
+public class SearchBar extends AppCompatActivity {
+    private boolean foucusing;
+    private ImageView search_button;
+    private EditText search_text;
+    private View search_content;
+
+    public SearchBar(ImageView search_button, EditText search_text, View search_content){
+        this.search_button = search_button;
+        this.search_text = search_text;
+        this.search_content = search_content;
+        SetFocusChangeListener();
+        foucusing = false;
+        DisableSearchBar();
+    }
+    public void Reversefocusing(){
+        if (foucusing){
+            setFocusing(false);
+        }
+        else {
+            setFocusing(true);
+        }
+    }
+    private void setFocusing(boolean bool){
+        foucusing = bool;
+        if (bool){
+            EnableSearchBar();
+        }
+        else {
+            DisableSearchBar();
+        }
+    }
+    public String getSearchText(){
+        return search_text.getText().toString();
+    }
+    private void DisableSearchBar(){
+        search_button.setImageResource(R.drawable.search);
+        search_text.setHint("点击搜索");
+        search_text.clearFocus();
+        search_content.setElevation(0);
+        search_content.setAlpha((float) 0.5);
+        Keyboard(false);
+    }
+    private void EnableSearchBar(){
+        search_button.setImageResource(R.drawable.search_color);
+        search_text.setHint("输入武将姓名");
+        search_text.requestFocus();
+        search_content.setElevation(5);
+        search_content.setAlpha((float) 0.8);
+        Keyboard(true);
+    }
+
+    private void Keyboard(boolean show){
+        InputMethodManager imm = (InputMethodManager) search_text.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (show){
+            imm.toggleSoftInput(0, InputMethodManager.SHOW_FORCED);
+        }//显示键盘
+        else {
+            imm.hideSoftInputFromWindow(search_text.getWindowToken(), 0) ;
+        }//收起键盘
+    }
+
+    private void SetFocusChangeListener(){
+        search_text.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (hasFocus&&!foucusing) {
+                    setFocusing(true);
+                }
+            }
+        });
+    }
+
+}
